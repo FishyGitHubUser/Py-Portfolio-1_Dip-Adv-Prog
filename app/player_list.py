462ae7af13bfc3725cf2e260c42db5621c629707
FishyGitHubUser <129918860+FishyGitHubUser@users.noreply.github.com>
2025-08-28 00:22:03 +0800

    feat(playerlist): add PlayerList class with private head initialized to None

---
+++ app/player_list.py
@@ -0,0 +1,10 @@
+"""
+Defines the PlayerList class, representing a list of players.
+
+The class utilises linked list functionality for handling player objects.
+"""
+
+
+class PlayerList:
+    def __init__(self, head=None):
+        self._head = head
